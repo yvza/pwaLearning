9945c7529717410915e1b8768f21d496bb6674df
yuza <akhmadyuza@gmail.com>
2021-10-11 18:59:04 +0700

    promise fetch

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -6,6 +6,9 @@ if ('serviceWorker' in navigator) {
     .register('/sw.js')
     .then(function () {
       console.log('Service worker registered!');
+    })
+    .catch(function(err) {
+      console.log(err);
     });
 }
 
@@ -16,8 +19,28 @@ window.addEventListener('beforeinstallprompt', function(event) {
   return false;
 });
 
-setTimeout(function() {
-  console.log('This is executed once the timer is done!');
-}, 3000);
+var promise = new Promise(function(resolve, reject) {
+  setTimeout(function() {
+    //resolve('This is executed once the timer is done!');
+    reject({code: 500, message: 'An error occurred!'});
+    //console.log('This is executed once the timer is done!');
+  }, 3000);
+});
+
+// promise.then(function(text) {
+//   return text;
+// }, function(err) {
+//   console.log(err.code, err.message)
+// }).then(function(newText) {
+//   console.log(newText);
+// });
+
+promise.then(function(text) {
+  return text;
+}).then(function(newText) {
+  console.log(newText);
+}).catch(function(err) {
+  console.log(err.code, err.message);
+});
 
 console.log('This is executed right after setTimeout()');
